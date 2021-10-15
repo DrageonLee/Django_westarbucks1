d71711c30c8e28b0d9e3d35027a3c7ca0e834204
Yonggeon Lee <yonggeonlee@Yonggeonui-MacBookAir.local>
2021-10-15 21:47:34 +0900

    Updated settings.py

--- westarbucks1/settings.py
+++ westarbucks1/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'products'
 ]
 
 MIDDLEWARE = [
