c54354e5f2eb98e5ede5f0f46e152aeaeb36871b
Yonggeon Lee <yonggeonlee@Yonggeonui-MacBookAir.local>
2021-10-15 21:26:14 +0900

    django initial settings

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks1',
+        'USER': 'root',
+        'PASSWORD': 'leeyonggeon',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+				'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+
+SECRET_KEY = 'django-insecure-=v0$1nxfzc@v)vt1d9dmsx0_#ice-t^6mw-lv&)06whtl2to$r'
